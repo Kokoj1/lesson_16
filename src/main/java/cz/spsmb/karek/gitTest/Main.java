c7d4d6278fa129646f67f5065dc3c331e2d8ec5d
marek.kubelka <marek.kubelka@student.spsmb.cz>
2023-12-20 11:33:07 +0100

    Task 2 - implemented second power of number.

--- src/main/java/cz/spsmb/karek/gitTest/Main.java
+++ src/main/java/cz/spsmb/karek/gitTest/Main.java
@@ -1,6 +1,6 @@
 package cz.spsmb.karek.gitTest;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World.");
     }
 }
